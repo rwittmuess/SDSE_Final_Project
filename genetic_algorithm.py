06d062613e1cfe623f7c1049119c8fbc86af081a
rwittmuess <wittmuess.richard@gmail.com>
2022-12-05 17:44:34 -0800

    created high-level GA structur

---
+++ genetic_algorithm.py
@@ -0,0 +1,52 @@
+'''
+
+    This file runs the Genetic Algorithm
+
+
+'''
+
+
+
+class Genetic_Algoritm():
+
+
+    def initialize_population():
+        '''
+        Initializing population
+
+        consider: potential input for seeding
+        '''
+
+
+    def fitness():
+        '''
+        Fitness funtion:
+            gets: individual 
+            returns: fitness score    
+
+        To consider:
+         * binary  
+         * non-binary 
+         * categorial
+        '''
+
+    def selecting_parents():
+        '''
+        Selecting Parents
+        '''
+
+
+    def mating():
+        '''
+        Mating
+        -Genertating new Individuals
+            -Crossover
+            -Mutation
+        
+        gets:       set/list/... of individuals
+        returns:    set/list/... of individuals with mutated genom
+        '''
+    
+
+
+
